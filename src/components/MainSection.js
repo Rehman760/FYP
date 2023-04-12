ee4d9b94112834e54debb67862ae1f6409dc4405
ameerparial <khuhroameerhamza4@gmail.com>
2023-04-13 00:14:07 +0500

    Donor and Student Login button navigation is set

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -12,7 +12,7 @@ const MainSection = () => {
             <h1 className="text-5xl font-bold mb-4 ">Finance for Education</h1>
             <p className="text-xl mb-8">We provide funding for students in need.</p>
 
-            <Link to="/LogIn">
+            <Link to="/donor">
             <button className="bg-green-500 text-white px-4 py-2 rounded-lg border-2 hover:bg-green-700">
               Become a Donor
             </button>

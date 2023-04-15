908aa9bb83e7c4d87167f26e5badd8fd7a57006d
ameerparial <khuhroameerhamza4@gmail.com>
2023-04-15 23:56:51 +0500

    Firebase folder is created

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {app} from "./FirebaseConfig";
+import {app} from "./Firebase/FirebaseConfig";
 import {getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LogIn = () => {

1d72d07d16865e8aabb5ecb31ca340ecd63cab72
Abdul Rehman <abdulrehmanq59@gmail.com>
2023-05-05 00:18:08 +0500

    "css added student navbar"

--- src/components/student/StudentNavbar.js
+++ src/components/student/StudentNavbar.js
@@ -3,6 +3,8 @@ import Hamburger from 'hamburger-react'
 import { StudentNavbarData,} from './StudentNavbarData';
 import { NavLink, Outlet } from 'react-router-dom';
 import { getProfileImage } from '../Firebase/SaveData';
+import logo from "../images/logo2.jpg"
+
 
 function StudentNavbar() {
     const [isOpenHumSign , setOpenHumSign] = useState(false);
@@ -19,28 +21,31 @@ function StudentNavbar() {
     return (
         <>
         <nav className="fixed w-full bg-white shadow">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between ">
+                <div className="flex items-center h-16">
                     <div>
                         <Hamburger toggled={isOpenHumSign} toggle={setOpenHumSign} size={20}/>
                     </div>
                     <div className="flex-shrink-0">
                         <h1 className="text-lg font-bold text-gray-800">Student Dashboard</h1>
                     </div>
-                    <div className="flex items-center">
+                
+                </div>
+                <div className="flex items-center">
                         <div className="ml-4 flex items-center">
 
                             <h2 className="ml-2 text-md font-medium text-gray-800">{`Hello, ${'ERROR'}`}</h2>
                             <img className="w-8 h-8 rounded-full border-2 border-green-500" src={URL} alt="User"/>
                         </div>
                     </div>
-                </div>
             </div>
         </nav>
         <div className="student-portal">
            <div style={{width: isOpenHumSign ? "250px" : "50px"}} className="side-nav-bar">
                <div className="logo-section">
-                   <h1 style={{display: isOpenHumSign ? "block" : "none"}} className="logo">Logo</h1>
+                   <h1 style={{display: isOpenHumSign ? "block" : "none"}} className="logo">
+                    <img className="rounded-full ml-8" src={logo} alt="logo"/>
+                   </h1>
                    <div style={{marginLeft: isOpenHumSign ? "85px" : "0px"}} className="ham-sign">
                        {/* <Hamburger toggled={isOpenHumSign} toggle={setOpenHumSign} size={20}/> */}
                    </div>

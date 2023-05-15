e278d1cc5c64cabfd68972fd977e2a7c845892b2
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-15 21:27:15 +0500

    class changed to className

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -106,7 +106,7 @@ const LogIn = ({role, myEmail}) => {
 
           {errorMessage &&  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
             {/* <strong class="font-bold">Error!</strong> */}
-            <span class="block sm:inline">{errorMessage}</span>
+            <span className="block sm:inline">{errorMessage}</span>
           </div>
           }
 

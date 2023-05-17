b3840a2fd62d408bb0ad3665683b0397c762292b
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-17 16:03:05 +0500

    Sponsored Students are shown

--- src/components/donor/SponsoredStudents.js
+++ src/components/donor/SponsoredStudents.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
+import { getStudentsDonated } from '../Firebase/SaveData';
 
-const SponsoredStudent = () => {
-  const donors = [
+const SponsoredStudent = ({donorEmail}) => {
+    const [students, setStudents] = useState();
+  
+  useEffect(function(){
+    getStudentsDonated(donorEmail, function(students){
+        console.log(students)
+        setStudents(students);
+    });
+
+  },[]);
+
+  return (
+    <Table responsive bordered hover>
+      <thead>
+        <tr className="bg-green-500 text-white">
+          <th>Student Name</th>
+          <th>University</th>
+          <th>Program</th>
+          <th>Sponsored</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students?.map((student, index) => (
+          <tr key={index}>
+            <td>{student.name}</td>
+            <td>${student.schoolName}</td>
+            <td>{student.program}</td>
+            <td>
+              {student.status === "approved" ? (
+                <FaCheckCircle className="text-green-500" />
+              ) : student.status === "pending" ? (
+                <FaTimesCircle className="text-yellow-500" />
+              ) : (
+                <FaTimesCircle className="text-red-500" />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default SponsoredStudent;
+
+/**
+ * const students = [
     {
       id: 1,
       name: "John Doe",
@@ -34,36 +80,4 @@ const SponsoredStudent = () => {
     }
   ];
 
-  return (
-    <Table responsive bordered hover>
-      <thead>
-        <tr className="bg-green-500 text-white">
-          <th>Student Name</th>
-          <th>University</th>
-          <th>Program</th>
-          <th>Sponsored</th>
-        </tr>
-      </thead>
-      <tbody>
-        {donors.map((donor) => (
-          <tr key={donor.id}>
-            <td>{donor.name}</td>
-            <td>${donor.amount}</td>
-            <td>{donor.studentName}</td>
-            <td>
-              {donor.status === "approved" ? (
-                <FaCheckCircle className="text-green-500" />
-              ) : donor.status === "pending" ? (
-                <FaTimesCircle className="text-yellow-500" />
-              ) : (
-                <FaTimesCircle className="text-red-500" />
-              )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-  );
-};
-
-export default SponsoredStudent;
+ */

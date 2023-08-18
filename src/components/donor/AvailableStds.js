e4c927953720de35ce72e0deecc45822adba923b
ameerparial <khuhroameerhamza4@gmail.com>
2023-08-18 15:38:58 +0500

    Create a table for showing the needy students to the donor

--- src/components/donor/AvailableStds.js
+++ src/components/donor/AvailableStds.js
@@ -1,60 +1,81 @@
-import { useEffect, useState } from "react";
-import { getAllStudents } from "../Firebase/SaveData";
-// import StudentView from "../StudentView";
-import ShowStudentView from "../student/ShowStudentView";
+import React, { useEffect, useState } from 'react';
+import { getAllStudents } from '../Firebase/SaveData';
+import { Link } from 'react-router-dom';
 
-export default function AvailableStds({donorEmail}){
-    const [students, setStudents] = useState([]);
-    const [category, setCategory] = useState('All');
+import { FaSearch } from 'react-icons/fa';
 
-    const handleCategoryChange = (event) => {
-        setCategory(event.target.value);
-    };
-    
-    useEffect(()=>{
-        getAllStudents(getStudentData);
-    },[]);
+const ResponsiveTable = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState([]);
 
-    const getStudentData = (students)=>{
-        console.log(students[0].imageUrl);
-        setStudents(students);
-    }
+  useEffect(function(){
+    getAllStudents(setData);
+  }, [])
 
-    
-    return (
-        <>
-        <div className="flex flex-col md:flex-row justify-between items-center py-4">
-          <div className="flex items-center md:mb-0">
-            <h2 className="text-2xl font-bold text-gray-800">Students</h2>
-            <div className="relative ml-2">
-              <select
-                className="block appearance-none w-full bg-white border border-green-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-green-300 focus:shadow-outline-green sm:text-sm"
-                value={category}
-                onChange={handleCategoryChange}
-              >
-                <option value="All">All</option>
-                <option value="Math">Math</option>
-                <option value="Science">Science</option>
-                <option value="History">History</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.95 7.95a.999.999 0 1 0-1.41-1.41l-3.54 3.54a.999.999 0 0 0-.29.71V16a1 1 0 1 0 2 0v-4.24l2.54-2.54z"/></svg>
-              </div>
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="p-6">
+    <h2 className="text-xl font-bold text-center mb-4">All Needy Students</h2>
+    <div className="overflow-x-auto">
+      <table className="min-w-full border rounded-lg shadow-md bg-white">
+        <thead>
+          <tr>
+            <th className="px-4 py-2" colSpan="6">
+            <div className="flex mb-4">
+          <div className="relative flex-1">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <FaSearch className="h-5 w-5 text-gray-500" />
             </div>
+            <input
+              type="text"
+              placeholder="Search by name, institution, or city"
+              className="w-full p-2 border rounded pr-10"
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+            
           </div>
-      
         </div>
+              
+              
+              
+            </th>
+          </tr>
+          <tr>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Institution</th>
+            <th className="px-4 py-2">Passing Year</th>
+            <th className="px-4 py-2">City</th>
+            <th className="px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map(item => (
+            <tr key={item.id} className="odd:bg-gray-100">
+              <td className="px-4 py-2">{item.name}</td>
+              <td className="px-4 py-2">{item.schoolName}</td>
+              <td className="px-4 py-2">{item.gradYear}</td>
+              <td className="px-4 py-2">{item.city}</td>
+              <td className="px-4 py-2 space-x-2">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded">
+                  <Link to={`/donor/student/${item.id}`} state={{stdEmail:item.email}}>View Profile</Link>
+                </button>
+                <button className="bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded">
+                  Donate
+                </button>
+                <button className="bg-purple-500 hover:bg-purple-600 text-white px-3 py-1 rounded">
+                  Chat
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+  );
+};
 
-
-        
-        <div className="container">
-            <h1 className="text-center my-3 font-medium text-3xl">{'List of all Students'}</h1>
-            <div className="row">
-                {students.map((student, index) => (
-                    <ShowStudentView key={index} id={index} name={student.name} bio={student.schoolName} picture={student.imageUrl} status={'Not Sponsored'} email={student.email} donorEmail={donorEmail}/>
-                ))}
-            </div>
-        </div>
-        </>
-    );
-}
+export default ResponsiveTable;

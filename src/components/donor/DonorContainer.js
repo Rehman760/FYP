a27802031298c7fe0f9da4e6ce42c097d228e01e
ameerparial <khuhroameerhamza4@gmail.com>
2023-08-18 15:39:22 +0500

    Added the table to donor dashboard

--- src/components/donor/DonorContainer.js
+++ src/components/donor/DonorContainer.js
@@ -4,13 +4,15 @@ import AvailableStds from './AvailableStds';
 import DonorProfile from './DonorProfile';
 import StudentTable from '../StudentTable';
 import SponsoredStudent from './SponsoredStudents';
+import ResponsiveTable from './AvailableStds';
+import Logout from '../Logout';
 
 function DonorContainer({donorEmail}) {
   const {donorID} = useParams();
   console.log(donorID);
 
   if(donorID==='record'){
-    return <AvailableStds donorEmail={donorEmail}/>
+    return <ResponsiveTable/>
   }
   else if(donorID ==='chat'){
     return <h1>I am a chat</h1>
@@ -21,6 +23,9 @@ function DonorContainer({donorEmail}) {
   }
   else if(donorID==='profile'){
     return <DonorProfile/>
+  }
+  else if(donorID==='logout'){
+    return <Logout/>
   }
 }
 export default DonorContainer;

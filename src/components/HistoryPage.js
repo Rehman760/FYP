c5756acbc6d164bb21afbd3ebf2bde7309c64903
ameerparial <khuhroameerhamza4@gmail.com>
2023-08-21 10:27:03 +0500

    Show the student profile in history page

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -6,6 +6,7 @@ const HistoryPage = (props) => {
         {
             id: 1,
             name: "Abdul Rehman",
+            email:'std1@gmail.com',
             bio: "Abdul Rehman is a hardworking student who is currently studying computer science at XYZ University. He is passionate about technology and wants to use his skills to help others.",
             picture: "https://via.placeholder.com/150",
             status: "sponsored"
@@ -13,6 +14,7 @@ const HistoryPage = (props) => {
         {
             id: 2,
             name: "Ameer Hamza",
+            email:'std2@gmail.com',
             bio: "Ameer is a dedicated student who is currently pursuing a degree in medicine at ABC University. He is committed to making a positive impact on her community through his profession.",
             picture: "https://via.placeholder.com/150",
             status: "sponsored"
@@ -20,6 +22,7 @@ const HistoryPage = (props) => {
         {
             id: 3,
             name: "Shoaib Ahmed",
+            email:'std3@gmail.com',
             bio: "Shoaib is a hardworking student who is currently studying computer science at XYZ University. He is passionate about technology and wants to use his skills to help others.",
             picture: "https://via.placeholder.com/150",
             status: "sponsored"
@@ -32,7 +35,7 @@ const HistoryPage = (props) => {
         <h1 className="text-center my-3 font-medium text-3xl">{props.title}</h1>
         <div className="row">
             {students.map((student, index) => (
-              <StudentView key={index} id={student.id} name={student.name} bio={student.bio} picture={student.picture} status={student.status} />
+              <StudentView key={index} id={student.id} name={student.name} bio={student.bio} picture={student.picture} status={student.status} email={student.email} />
             ))}
         </div>
     </div>
--- src/components/StudentView.js
+++ src/components/StudentView.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function StudentView({id,picture,name,bio,status}) {
+function StudentView({id,picture,name,bio,status,email}) {
   return (
     <div className="col-sm-4 my-2" key={id}>
     <Card>
@@ -12,7 +13,11 @@ function StudentView({id,picture,name,bio,status}) {
             <Card.Title>{name}</Card.Title>
             <Card.Text>{bio}</Card.Text>
                 <Card.Text className="text-muted">Status: {status}</Card.Text>
-            <Button variant="success" href={`/student/${id}`}>View Profile</Button>
+            <Button variant="success">
+                <Link to={`/donor/student/${id}`} state={{stdEmail:email}} className='success'>
+                    View Profile
+                </Link>
+            </Button>
         </Card.Body>
     </Card>
 </div>

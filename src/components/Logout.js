e83275217fbc96be99eb65edf65a2d9e7c293d18
ameerparial <khuhroameerhamza4@gmail.com>
2023-08-22 08:22:43 +0500

    Change handler name from login to logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Logout = () => {
-    const handleLogin = () => {
+    const handleLogout = () => {
         sessionStorage.removeItem('studentEmail');
         sessionStorage.removeItem('donorEmail');
+        sessionStorage.removeItem('universityEmail');
         // Replace the current route with a new route (e.g., your login page)
         window.history.replaceState(null, '', '/');
         // Reload the page to reflect the new URL
@@ -18,7 +19,7 @@ const Logout = () => {
         <h1 className="text-2xl font-semibold mb-4">Logout</h1>
         <p className="text-gray-600 mb-4">Are you sure you want to log out?</p>
         <div className="flex justify-center">
-          <button onClick={handleLogin} className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded mr-4" >
+          <button onClick={handleLogout} className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded mr-4" >
             Logout
           </button>
           <button onClick={handleCancel} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 py-2 rounded">

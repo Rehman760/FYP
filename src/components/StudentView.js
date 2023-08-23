005142ba2453373e96df05047b08d32ab7620c3b
ameerparial <khuhroameerhamza4@gmail.com>
2023-08-23 12:28:34 +0500

    Buttons are set in the landing page

--- src/components/StudentView.js
+++ src/components/StudentView.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function StudentView({id,picture,name,bio,status,email}) {
+    const navigate = useNavigate();
   return (
     <div className="col-sm-4 my-2" key={id}>
     <Card>
@@ -13,11 +14,11 @@ function StudentView({id,picture,name,bio,status,email}) {
             <Card.Title>{name}</Card.Title>
             <Card.Text>{bio}</Card.Text>
                 <Card.Text className="text-muted">Status: {status}</Card.Text>
-            <Button variant="success">
+            <button variant='success' className="w-full h-12 font-bold  text-green-500 border border-green-100 rounded-lg hover:text-white-500 hover:bg-green-600 hover:text-white">
                 <Link to={`/donor/student/${id}`} state={{stdEmail:email}} className='success'>
                     View Profile
                 </Link>
-            </Button>
+            </button>
         </Card.Body>
     </Card>
 </div>

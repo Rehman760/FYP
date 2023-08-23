7526077bb3a91d2fe4956655b41468c45f9051ea
Rehman760 <abdulrehmanq59@gmail.com>
2023-08-23 15:48:08 +0500

    "icons changed"

--- src/components/student/StudentNavbarData.js
+++ src/components/student/StudentNavbarData.js
@@ -1,79 +1,75 @@
-import { FaAccessibleIcon, FaBookOpen, FaCalendar, FaDelicious, FaInbox, FaLock, FaMoneyBill, FaRegistered } from 'react-icons/fa';
-import Dashboard from './Dashboard';
-import StudentProfile from './StudentProfile';
-import Notification from '../Notification';
-import StudentInformation from './StudentInformation';
-import UserProfile from './UserProfile';
-import Logout from '../Logout';
-import ChatList from '../ChatList';
-import PaymentNotifcation from './PaymentNotification';
-
-// let email='';
-
-// export function setMyEmail(myemail){
-//     email = myemail;
-// }
-
-// export function getMyEmail(){
-//     return email;
-// }
-
-export const StudentNavbarData = [
+import {
+    FaTachometerAlt, // Dashboard
+    FaComment,       // Chats
+    FaUserPlus,      // Register
+    FaMoneyBill,     // Payment
+    FaBell,          // Notifications
+    FaUser,          // View Profile
+    FaUserEdit,      // Edit Profile
+    FaSignOutAlt,    // Logout
+  } from 'react-icons/fa';
+  
+  import Dashboard from './Dashboard';
+  import StudentProfile from './StudentProfile';
+  import Notification from '../Notification';
+  import StudentInformation from './StudentInformation';
+  import UserProfile from './UserProfile';
+  import Logout from '../Logout';
+  import ChatList from '../ChatList';
+  import PaymentNotifcation from './PaymentNotification';
+  
+  export const StudentNavbarData = [
     {
-        title:'Dashboard',
-        icons:<FaDelicious/>,
-        link:'dashboard',
-        body: <Dashboard/>
-
+      title: 'Dashboard',
+      icons: <FaTachometerAlt />,
+      link: 'dashboard',
+      body: <Dashboard />,
     },
     {
-        title:'Chats',
-        icons:<FaCalendar/>,
-        link:'chat',
-        body: <ChatList/>
+      title: 'Chats',
+      icons: <FaComment />,
+      link: 'chat',
+      body: <ChatList />,
     },
     {
-        title:'Register',
-        icons:<FaRegistered/>,
-        link:'form',
-        body: <StudentInformation/>
+      title: 'Register',
+      icons: <FaUserPlus />,
+      link: 'form',
+      body: <StudentInformation />,
     },
     {
-        title:'Payment',
-        icons:<FaMoneyBill/>,
-        link:'payment',
-        body:<PaymentNotifcation/>
+      title: 'Payment',
+      icons: <FaMoneyBill />,
+      link: 'payment',
+      body: <PaymentNotifcation />,
     },
     {
-        title:'Notifications',
-        icons:<FaInbox/>,
-        link:'notification',
-        body:<Notification/>
+      title: 'Notifications',
+      icons: <FaBell />,
+      link: 'notification',
+      body: <Notification />,
     },
-
     {
-        title:'View Profile',
-        icons:<FaBookOpen/>,
-        link:'information',
-        body:<StudentProfile/>
+      title: 'View Profile',
+      icons: <FaUser />,
+      link: 'information',
+      body: <StudentProfile />,
     },
-
     {
-        title:'Edit Profile',
-        icons:<FaAccessibleIcon/>,
-        link:'profile',
-        body:<UserProfile/>
+      title: 'Edit Profile',
+      icons: <FaUserEdit />,
+      link: 'profile',
+      body: <UserProfile />,
     },
-
     {
-        title:'Logout',
-        icons:<FaLock/>,
-        link:'logout',
-        body:<Logout/>
-    }
-]
-
-export function getBody(searchlink){
-    return StudentNavbarData.find(({link})=> link===searchlink)
-
-}
+      title: 'Logout',
+      icons: <FaSignOutAlt />,
+      link: 'logout',
+      body: <Logout />,
+    },
+  ];
+  
+  export function getBody(searchlink) {
+    return StudentNavbarData.find(({ link }) => link === searchlink);
+  }
+  

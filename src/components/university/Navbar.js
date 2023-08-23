252bead4f56ab840d9f7adfc3d8e81a0987ccd22
Rehman760 <abdulrehmanq59@gmail.com>
2023-08-23 16:57:33 +0500

    "navbar set"

--- src/components/university/Navbar.js
+++ src/components/university/Navbar.js
@@ -1,20 +1,62 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { getDonorsStudents } from '../Firebase/SaveData';
+import { FaPlus, FaList, FaHome, FaSignOutAlt } from 'react-icons/fa'; // Import icons
 
 function Navbar() {
   const name = sessionStorage.getItem('universityName');
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <nav className="bg-green-500 p-4 flex items-center justify-between">
-      <div>
+    <nav className="bg-green-500 p-4 md:flex md:items-center md:justify-between">
+      <div className="flex items-center">
         <span className="text-white font-bold text-xl">University</span>
+        <span className="text-white ml-4">{name}</span>
       </div>
-      <div>
-        <span className="text-white mr-4">{name}</span>
-        <button onClick={(e)=>{}} className="bg-white text-green-500 font-bold py-2 px-4 rounded-lg">
-          <NavLink to={'logout'}>Log Out</NavLink>
-        </button>
+
+      <div className="md:flex md:items-center space-x-4">
+        {/* Hamburger Menu Icon */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white">
+            ☰
+          </button>
+        </div>
+
+        {/* Navigation Links (Hidden on Small Screens) */}
+        <div className={`md:flex space-x-4 ${menuOpen ? 'block' : 'hidden'}`}>
+          {/* Home */}
+          <NavLink to={'/'} className="bg-white text-green-500 font-bold py-2 px-4 rounded-lg">
+           
+            Home
+          </NavLink>
+
+          {/* Add Programs */}
+          <NavLink to={'add-programs'} className="bg-white text-green-500 font-bold py-2 px-4 rounded-lg">
+           
+            Add Programs
+          </NavLink>
+
+          {/* View Students List */}
+          <NavLink to={'view-students'} className="bg-white text-green-500 font-bold py-2 px-4 rounded-lg">
+           
+            View Students List
+          </NavLink>
+          <div className="">
+          <button
+            onClick={(e) => {}}
+            className="bg-white text-green-500 font-bold py-2 px-4 rounded-lg"
+          >
+            <NavLink to={'logout'} className="text-green-500">
+              Log Out
+            </NavLink>
+          </button>
+        </div>
+        </div>
+
+   
+       
       </div>
     </nav>
   );
